925d0aef5e948f0a5acb1d8a32111a59bd82ea15
bkis <kiss@mailbox.org>
2017-05-03 18:03:17 +0200

    init

---
+++ batcave/src/test/java/de/uni/koeln/spinfo/bkiss/batcave/BatcaveApplicationTests.java
@@ -0,0 +1,16 @@
+package de.uni.koeln.spinfo.bkiss.batcave;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BatcaveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
