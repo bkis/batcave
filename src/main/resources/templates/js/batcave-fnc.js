837e83247c69e044ec1e8b23f7b09ffdf625ace5
bkis <kiss@mailbox.org>
2017-05-09 15:36:26 +0200

    prototyped mouse hovering functionality

--- src/main/resources/templates/js/batcave-fnc.js
+++ src/main/resources/templates/js/batcave-fnc.js
@@ -1,10 +1,15 @@
-function getColorFor(tag){
-	
+var colorHash = new ColorHash({lightness: 0.7});
+
+
+function getColorFor(tagString){
+	return colorHash.hex(tagString.length + tagString);
 }
+
 
 function getJsonQ(jsonString){
 	return jsonQ(JSON.parse(jsonString));
 }
+
 
 function getLegend(jsonQ){
 	//extract tags
@@ -14,10 +19,21 @@ function getLegend(jsonQ){
 	//build legend json
 	var legend = {};
 	for (var i = 0; i < tags.length; i++) {
-		legend[tags[i]] = "#f00";
+		legend[tags[i]] = getColorFor(tags[i]);
 	}
 
 	//return legend
 	return legend;
 }
 
+
+function getDisplayData(jsonQ){
+	var data = jsonQ.find("objects").value()[0];
+	var out = "";
+	
+	for (obj in data){
+		out += '<span class="bc-object" data-tag="' + data[obj].tags + '">' + data[obj].display + '</span>';
+	}
+	
+	return out;
+}
--- src/main/resources/templates/js/batcave.js
+++ src/main/resources/templates/js/batcave.js
@@ -4,17 +4,29 @@ $(document).ready(function() {
 	var jsonString = '{"meta":{"volume":"Volume1","chapter":"KapitelDings","page":"32"},"objects":[{"display":"Haus","tags":["NN","ADV","V_PP"]},{"display":"bauen","tags":["V_PP"]}]}';
 	var jsonQ = getJsonQ(jsonString);
 
-	//generate legend data object
+	//extract and prepare data
 	var legend = getLegend(jsonQ);
 
 	//fill legend in DOM
+	$("#bc-legend").empty();
 	for (var key in legend) {
 		$("#bc-legend").append('<span class="bc-legend-item" data-tag="' + key + '">' + key + '</span>');
 	}
+	
 	//set legend colors
 	$(".bc-legend-item").each(function() {
 		$( this ).css( "border-color", legend[$(this).attr("data-tag")] );
 	});
+	
+	//set hover actions for legend items
+	$(".bc-legend-item").hover(function() {
+		$(".bc-object[data-tag='" + $(this).attr("data-tag") + "']").css("background-color", legend[$(this).attr("data-tag")]);
+	}, function() {
+		$(".bc-object[data-tag='" + $(this).attr("data-tag") + "']").css("background-color", "#fff");
+	});
+	
+	//fill display
+	$("#bc-display").append(getDisplayData(jsonQ));
 
 
 });
