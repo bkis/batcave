903a374f9b38bcdbc8fda13433ec57eff7c87a2d
bkis <kiss@mailbox.org>
2017-05-10 11:55:09 +0200

    joined project js-files

--- src/main/resources/templates/js/batcave.js
+++ src/main/resources/templates/js/batcave.js
@@ -1,3 +1,45 @@
+var colorHash = new ColorHash({lightness: 0.7});
+
+
+function getColorFor(tagString){
+	return colorHash.hex(tagString);
+}
+
+
+function getJsonQ(json){
+	return jsonQ(json);
+}
+
+
+function getTags(json){
+	var jsonQ = getJsonQ(json);
+	//extract tags
+	var tags = jsonQ.find("tags").value();
+	tags = Array.from(new Set([].concat.apply([], tags)));
+
+	//build tags json
+	var tagsData = {};
+	for (var i = 0; i < tags.length; i++) {
+		tagsData[tags[i]] = getColorFor(tags[i]);
+	}
+
+	//return tags data
+	return tagsData;
+}
+
+
+function getDisplayData(json){
+	var out = "";
+	
+	for (obj in json.objects){
+		out += '<span class="bc-object" data-tag="' + 
+				json.objects[obj].tags + '">' + 
+				json.objects[obj].display + '</span>';
+	}
+	
+	return out;
+}
+
 
 $(document).ready(function() {
 
