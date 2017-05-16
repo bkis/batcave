adfe83dbbbcd10d8d32c99bc204a21c3370a9793
bkis <kiss@mailbox.org>
2017-05-16 16:45:56 +0200

    db transform wip

--- src/main/java/de/uni/koeln/spinfo/bkiss/batcave/db/transform/DbTransform.java
+++ src/main/java/de/uni/koeln/spinfo/bkiss/batcave/db/transform/DbTransform.java
@@ -5,10 +5,16 @@ import java.io.InputStream;
 import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.bson.Document;
 
 import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.ReadContext;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.Configuration.ConfigurationBuilder;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
@@ -40,6 +46,10 @@ public class DbTransform {
 		int port = Integer.valueOf(props.getProperty("port"));
 		MongoClient mongo = new MongoClient(host, port);
 		
+		//set logging
+		Logger.getLogger("org.mongodb.driver.protocol.command").setLevel(Level.SEVERE);
+		Logger.getLogger("com.jayway.jsonpath.internal.path.CompiledPath").setLevel(Level.SEVERE);
+		
 		//start transformation
 		try {
 			transform(
@@ -58,25 +68,33 @@ public class DbTransform {
 	
 	
 	private static void transform(MongoDatabase s, MongoDatabase t) throws Exception {
+		
 		//tokens list
 		Set<PageDocument> targetPages = new HashSet<PageDocument>();
+		
 		//get ARC words collection interator
 		MongoCursor<Document> words = s.getCollection("words").find().iterator();
+		
 		//get ARC pages, chapters, volumes and languages Collections
 		MongoCollection<Document> pages = s.getCollection("pages");
 		MongoCollection<Document> chapters = s.getCollection("chapters");
 		MongoCollection<Document> volumes = s.getCollection("volumes");
 		MongoCollection<Document> languages = s.getCollection("languages");
+		
 		//collect token data and generate PageDocument instances
+		int count = 0;
 		try {
-		    while (words.hasNext()) {
-		    	String json = words.next().toJson();
-		        System.out.println((String)JsonPath.read(json, "$.versions[0].version"));
+		    while (words.hasNext() && count < 10) {
+		    	ReadContext json = JsonPath.parse(words.next().toJson());	//get json object
+		    	//TODO iterate words, construct pagedocuments
+		        System.out.println((Object)json.read("$.versions"));
+		        count++;
 		    }
 		} finally {
 		    words.close();
 		}
+		
 	}
-
+	
 	
 }
