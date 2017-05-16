33d6df3ed7e079c0796a2dc33869b5a90535cfc6
bkis <kiss@mailbox.org>
2017-05-16 15:08:32 +0200

    prototyped db transformation procedure

--- src/main/java/de/uni/koeln/spinfo/bkiss/batcave/db/transform/DbTransform.java
+++ src/main/java/de/uni/koeln/spinfo/bkiss/batcave/db/transform/DbTransform.java
@@ -2,14 +2,19 @@ package de.uni.koeln.spinfo.bkiss.batcave.db.transform;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 
 import org.bson.Document;
 
 import com.jayway.jsonpath.JsonPath;
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+
+import de.uni.koeln.spinfo.bkiss.batcave.db.data.PageDocument;
 
 /**
  * Class used to transform data from the ARC project db into a new purpose-built db structure
@@ -53,16 +58,20 @@ public class DbTransform {
 	
 	
 	private static void transform(MongoDatabase s, MongoDatabase t) throws Exception {
-		//get ARC collections
+		//tokens list
+		Set<PageDocument> targetPages = new HashSet<PageDocument>();
+		//get ARC words collection interator
 		MongoCursor<Document> words = s.getCollection("words").find().iterator();
-		
-		int count = 0;
-		
+		//get ARC pages, chapters, volumes and languages Collections
+		MongoCollection<Document> pages = s.getCollection("pages");
+		MongoCollection<Document> chapters = s.getCollection("chapters");
+		MongoCollection<Document> volumes = s.getCollection("volumes");
+		MongoCollection<Document> languages = s.getCollection("languages");
+		//collect token data and generate PageDocument instances
 		try {
-		    while (words.hasNext() && count < 100) {
+		    while (words.hasNext()) {
 		    	String json = words.next().toJson();
 		        System.out.println((String)JsonPath.read(json, "$.versions[0].version"));
-		        count++;
 		    }
 		} finally {
 		    words.close();
