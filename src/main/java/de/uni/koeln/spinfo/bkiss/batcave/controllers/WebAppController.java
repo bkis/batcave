8f5fa8c2cffcd11d3a88d8c58e653ba3dd44407f
bkis <kiss@mailbox.org>
2017-05-19 19:21:41 +0200

    prototyped page view

--- src/main/java/de/uni/koeln/spinfo/bkiss/batcave/controllers/WebAppController.java
+++ src/main/java/de/uni/koeln/spinfo/bkiss/batcave/controllers/WebAppController.java
@@ -1,7 +1,24 @@
 package de.uni.koeln.spinfo.bkiss.batcave.controllers;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.jayway.jsonpath.JsonPath;
+import com.mongodb.BasicDBObject;
+import com.mongodb.util.JSON;
+
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
 
 @Controller
 public class WebAppController {
@@ -15,5 +32,45 @@ public class WebAppController {
     public String defaultRequest() {
         return "index";
     }
-
+    
+    @RequestMapping("/page")
+    public String pageView(
+    		@RequestParam(value="id", required=true, defaultValue="error") String id,
+    		Model model) {
+    	
+    	//load dummy data
+    	Scanner fr = null;
+    	try {
+    		File file = new ClassPathResource("dummydata.json").getFile();
+			fr = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	
+    	StringBuilder sb = new StringBuilder();
+    	while (fr.hasNextLine())
+    		sb.append(fr.nextLine() + "\n");
+    	
+    	fr.close();
+    	
+//    	Gson gson = new GsonBuilder().create();
+//    	model.addAttribute("page", gson.fromJson(sb.toString(), PageDocument.class));
+    	
+    	BasicDBObject page = (BasicDBObject) JSON.parse(sb.toString());
+    	
+    	//add page id
+    	model.addAttribute("id", id);
+    	//add page object
+    	model.addAttribute("page", page);
+    	//add page title string
+    	model.addAttribute("title", "Seitenansicht");
+    	//add tags array
+    	JSONArray tags = JsonPath.read(page, "$.tokens[*].tags[0]");
+    	model.addAttribute("tags", new HashSet<String>(Arrays.asList(tags.toArray(new String[0]))));
+    	
+        return "page";
+    }
+    
 }
--- src/main/resources/static/js/batcave.js
+++ src/main/resources/static/js/batcave.js
@@ -2,92 +2,82 @@
 var colorHash = new ColorHash({lightness: 0.6});
 
 //returns color based on tag-string
-function getColorFor(tagString){
+function col(tagString){
 	return colorHash.hex(tagString);
 }
 
 //returns a json-object that maps tag names to colors
-function getTags(json){
-	var json = jsonQ(json);
-	//extract tags
-	var tags = json.find("tags").value();
-	tags = Array.from(new Set([].concat.apply([], tags)));
-
-	//build tags json
-	var tagsData = {};
-	for (var i = 0; i < tags.length; i++) {
-		tagsData[tags[i]] = getColorFor(tags[i]);
-	}
-
-	//return tags data
-	return tagsData;
-}
+//function getTags(json){
+//	var json = jsonQ(json);
+//	//extract tags
+//	var tags = json.find("tags").value();
+//	tags = Array.from(new Set([].concat.apply([], tags)));
+//
+//	//build tags json
+//	var tagsData = {};
+//	for (var i = 0; i < tags.length; i++) {
+//		tagsData[tags[i]] = getColorFor(tags[i]);
+//	}
+//
+//	//return tags data
+//	return tagsData;
+//}
 
 //builds the html-data for the text to display
-function getDisplayData(json){
-	var out = "";
-	var i = 0;
-	for (obj in json.objects){
-		out += '<span class="bc-object" data-locked="false" data-index="' + i + '" data-tag="' + 
-				json.objects[obj].tags + '">' + 
-				json.objects[obj].display + '</span>';
-		i++;
-	}
-	
-	return out;
-}
+//function getDisplayData(json){
+//	var out = "";
+//	var i = 0;
+//	for (obj in json.objects){
+//		out += '<span class="bc-object" data-locked="false" data-index="' + i + '" data-tag="' + 
+//				json.objects[obj].tags + '">' + 
+//				json.objects[obj].display + '</span>';
+//		i++;
+//	}
+//	
+//	return out;
+//}
 
 //show position in scan
-function showScanPosition(coordinates){
-	if (coordinates == false){
-		$("#scanbox").remove();
-		return;
-	}
-	if (typeof coordinates == "undefined"){
-		return;
-	}
+function showScanPosition(x, y, w, h){
 	//draw box
 	$("#bc-extra").prepend(
 		'<div id="scanbox"></div>'
 	);
 	//set position and dimensions
-	$("#scanbox").css("top",coordinates.y);
-	$("#scanbox").css("left",coordinates.x);
-	$("#scanbox").css("width",coordinates.width);
-	$("#scanbox").css("height",coordinates.height);
+	$("#scanbox").css("top", y);
+	$("#scanbox").css("left", x);
+	$("#scanbox").css("width", w);
+	$("#scanbox").css("height", h);
+}
+//hide position in scan
+function hideScanBox(){
+	$("#scanbox").remove();
 }
 
-//load json data from url
-function loadJsonData(url){
-	//load data
-	$.getJSON(url, function(data) {
-		prepareFrontend(data);
-	});
-}
 
 //build frontend elements based on data
 function prepareFrontend(json){
 	//extract and prepare data
-	var tags = getTags(json);
+//	var tags = getTags(json);
 
 	//fill tags area in DOM
-	$("#bc-tags").children(".bc-tags-item").detach();
-	for (var key in tags) {
-		$("#bc-tags").append('<span class="bc-tags-item" data-tag="' + key + '" data-locked="false">' + key + '</span>');
-	}
+//	$("#bc-tags").children(".bc-tags-item").detach();
+//	for (var key in tags) {
+//		$("#bc-tags").append('<span class="bc-tags-item" data-tag="' + key + '" data-locked="false">' + key + '</span>');
+//	}
 	
 	//fill display
-	$("#bc-display").append(getDisplayData(json));
+	//$("#bc-display").append(getDisplayData(json));
 	
 	//set tags colors
 	$(".bc-tags-item").each(function() {
-		$( this ).css( "border-color", tags[$(this).attr("data-tag")] );
+		$( this ).css( "border-color", col($(this).attr("data-tag")) );
 	});
 	
 	//set hover actions for tags items
 	$(".bc-tags-item").hover(function() {
 		$(this).addClass("bc-tags-item-hover");
-		$(".bc-object[data-tag*='" + $(this).attr("data-tag") + "']").css("background-color", tags[$(this).attr("data-tag")]);
+		$(".bc-object[data-tag*='" + $(this).attr("data-tag") + "']").css("background-color", col($(this).attr("data-tag")) );
 	}, function() {
 		if ($(this).attr("data-locked") == "false"){
 			$(this).removeClass("bc-tags-item-hover");
@@ -120,8 +110,13 @@ function prepareFrontend(json){
 				$(this).mouseenter();
 			}
 		});
-		var index = parseInt(target.attr("data-index"));
-		showScanPosition(json.objects[index].coordinates);
+		//var index = parseInt(target.attr("data-index"));
+		showScanPosition(
+				$(this).attr("data-scan-x"),
+				$(this).attr("data-scan-y"),
+				$(this).attr("data-scan-w"),
+				$(this).attr("data-scan-h")
+		);
 	}, function() {
 		var target = $(this);
 		$(".bc-tags-item").each(function(){
@@ -150,6 +145,6 @@ function prepareFrontend(json){
 
 //jQuery execute when doc fully loaded
 $(document).ready(function() {
-	loadJsonData("dummydata/document.json");
+	prepareFrontend();
 });
 
