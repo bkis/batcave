3ac15c25ec5671f688affaab2f72cc2e2867688a
bkis <kiss@mailbox.org>
2017-07-11 14:46:21 +0200

    js documentation

--- src/main/resources/static/js/batcave-controls.js
+++ src/main/resources/static/js/batcave-controls.js
@@ -1,14 +1,20 @@
 //jQuery execute when doc fully loaded
 $(document).ready(function() {
 	
+	//system maintenance command button behavior
 	$(".controls-btn").click(function() {
+		//quit if button disabled
 		if ($(this).hasClass("controls-btn-disabled")){
 			return;
 		}
-		var btn = $(this);
+		//disable button visually
 		$(this).addClass("controls-btn-disabled");
+		//set wait text
 		$(this).parent().parent().find(".controls-result").text("Bitte warten...");
+		//set loading icon
 		$(this).parent().parent().find(".controls-icon").attr("src", "/img/icon-loading.gif");
+		//send request, show result string when finished
+		var btn = $(this);
 		$(this).parent().parent().find(".controls-result").load(
 				"/actions",
 				{ action: $(this).attr("data-action") },
