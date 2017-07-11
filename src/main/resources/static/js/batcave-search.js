dda2e530bdcb7922f3dd002c97b5d712def8792b
bkis <kiss@mailbox.org>
2017-07-11 14:43:39 +0200

    js documentation

--- src/main/resources/static/js/batcave-search.js
+++ src/main/resources/static/js/batcave-search.js
@@ -1,6 +1,6 @@
 //jQuery execute when doc fully loaded
 $(document).ready(function() {
-	
+	//set click event for search result line
 	$(".bc-result").click(function() {
 		window.location = "page?id=" + $(this).attr("data-id") + "&hl=" + $(this).attr("data-index");
 	});
--- src/main/resources/static/js/batcave.js
+++ src/main/resources/static/js/batcave.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-	
+	//erase input values when reset button clicked
 	$("#bc-search-reset-btn").click(function(){
 		 $("#search-form").find('input:text').val('');
 	     return false;
 	});
 	
+	//start similarities search when sim.search button clicked
 	$("#bc-search-similarities-btn").click(function(){
 		window.location = "search-similarities?token=" + $("#search-form").find('#search-token').val();
 	});
